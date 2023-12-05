b6b9897d5a55e615f97deccba79905985c165d7b
laumonmay <laumonmay@hotmail.com>
2023-12-05 20:55:23 +0100

    various changes

--- js/main.js
+++ js/main.js
@@ -9,16 +9,25 @@ const addElement = document.querySelector('.js-btn-add');
 const input_Search_desc = document.querySelector('.js_in_search_desc');
 const button_search = document.querySelector('.js_btn_search');
 
-
+const kittenImage1 = 'https://dev.adalab.es/gato-siames.webp';
+const kittenName1 = 'Anastacio';
 const kittenDesc1 = `Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos
 hace 500 años, donde tuvo su origen muy posiblemente.`;
+const kittenBreed1 = 'Siamés';
+
+const kittenImage2 = 'https://dev.adalab.es/sphynx-gato.webp';
+const kittenName2 = 'Fiona';
 const kittenDesc2 = `Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
 hasta con pinta de alienígena han llegado a definir a esta raza
 gatuna que se caracteriza por la «ausencia» de pelo.`;
+const kittenBreed2 = 'Sphynx';
+
+const kittenImage3 = 'https://dev.adalab.es/maine-coon-cat.webp';
+const kittenName3 = 'Cielo';
 const kittenDesc3 = `  Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
 bella mirada se ha convertido en una de sus señas de identidad.
 Sus ojos son grandes y las orejas resultan largas y en punta.`;
-
+const kittenBreed3 = 'Maine Coon';
 
 /* Agregar el código del li desde HTMl 
 Repetir este proceso por cada gatito */
@@ -26,13 +35,12 @@ const kittenOne = `<li class="card">
 <article>
   <img
     class="card_img"
-    src="https://dev.adalab.es/gato-siames.webp"
+    src="${kittenImage1}"
     alt="gatito"
   />
-  <h3 class="card_title">Anastacio</h3>
-  <h4 class="card_race">Siamés</h4>
-  <p class="card_description">${kittenDesc1}
-   </p>
+  <h3 class="card_title">${kittenName1}</h3>
+  <h4 class="card_race">${kittenBreed1}</h4>
+  <p class="card_description">${kittenDesc1}</p>
 </article>
 </li>`;
 
@@ -43,24 +51,22 @@ const kittenTwo = `<li class="card">
 <article>
 <img 
 class="card_img" 
-src="https://dev.adalab.es/sphynx-gato.webp" 
+src="${kittenImage2}" 
 alt="sphynx-cat" />
-<h3 class="card_title">Fiona</h3>
-<h4 class="card_race">Sphynx</h4>
-<p class="card_description">${kittenDesc2} 
-</p>
+<h3 class="card_title">${kittenName2}</h3>
+<h4 class="card_race">${kittenBreed2}</h4>
+<p class="card_description">${kittenDesc2}</p>
 </article>
 </li>`;
 
 const kittenThree = `<li class="card">
 <article>
 <img class="card_img" 
-src="https://dev.adalab.es/maine-coon-cat.webp" 
+src="${kittenImage3}" 
 alt="maine-coon-cat" />
-<h3 class="card_title">Cielo</h3>
-<h4 class="card_race">Maine Coon</h4>
-<p class="card_description">${kittenDesc3}
-</p>
+<h3 class="card_title">${kittenName3}</h3>
+<h4 class="card_race">${kittenBreed3}</h4>
+<p class="card_description">${kittenDesc3}</p>
 </article>
 </li>`;
 
@@ -68,13 +74,9 @@ elementList.innerHTML = kittenOne + kittenTwo + kittenThree;
 
 form.classList.add('collapsed');
 
-addKitten.addEventListener('click', (event) => {
-  form.classList.remove('collapsed');
-});
+addKitten.addEventListener('click', handleClickNewCatForm);
 
-cancel.addEventListener('click', (event) => {
-  form.classList.add('collapsed');
-});
+cancel.addEventListener('click', hideNewCatForm);
 
 addElement.addEventListener('click', (event) => {
   event.preventDefault();
@@ -132,3 +134,16 @@ button_search.addEventListener('click', (event) => {
   console.log(descrSearchText);
 });
 
+
+function hideNewCatForm  () {
+  form.classList.add('collapsed');
+}
+
+function handleClickNewCatForm(event) {
+  event.preventDefault();
+  if (form.classList.contains('collapsed')) {
+    form.classList.remove('collapsed');
+  } else {
+    form.classList.add('collapsed');
+  }
+}
